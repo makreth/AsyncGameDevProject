589636a53d5c3cb2b811b0a45f0a01e989a6ff47
Ethan Amar Bechtol <48114346+EthanBechtol@users.noreply.github.com>
2021-11-29 20:37:00 -0800

    Add dialogue UI and trigger prefab

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static bool isDialogueActive = false;
+    public GameObject dialogueCanvas;
+    public Text speakerText;
+    public Text dialogueText;
+    public Text continueText;
+    public float continueFlashSpeed = 0.7f;
+
+    void Update()
+    {
+        if (isDialogueActive)
+        {
+            continueText.color = new Color(255, 255, 255, Mathf.PingPong(Time.unscaledTime * continueFlashSpeed, 1f));
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    public void TriggerDialogue(string speaker, string text)
+    {
+        isDialogueActive = true;
+        dialogueText.text = text;
+        speakerText.text = speaker;
+        dialogueCanvas.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void ResumeGame()
+    {
+        isDialogueActive = false;
+        dialogueCanvas.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public bool isSingleUse = false;
+    private bool used = false;
+    public DialogueManager dialogueManager;
+    public string speaker;
+    public string dialogue;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isSingleUse || (isSingleUse && !used))
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                used = true;
+                dialogueManager.TriggerDialogue(speaker, dialogue);
+            }
+        }
+    }
+}
