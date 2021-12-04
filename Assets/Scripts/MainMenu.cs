5fa3904e9b6b1ce0926a0745a007e443e002aba7
Ethan Amar Bechtol <48114346+EthanBechtol@users.noreply.github.com>
2021-12-04 10:01:57 -0800

    Update main menu with new mechanics

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,25 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void PlayGame()
     {
-        Debug.Log("Play game button not implemented.");
-        //SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("FinalLevelComposite");
     }
 
     public void QuitGame()
